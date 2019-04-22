76864d8555d0ccc8a01d72cc92fa602b44c5adf9
Serhii Sydoruk <serhiis@wix.com>
2019-04-22 15:11:30 +0300

    add two dumb methods (for collecting video data in two ways)

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,8 @@ const { spawn } = require('child_process');
 const program = require('commander');
 const updateNotifier = require('update-notifier');
 const pkg = require('../package.json');
+const videoWithWebRTC = require('./videoWithWebRTC');
+const traceWithScreenshots = require('./traceWithScreeshots');
 
 const handleError = require('./handler-error');
 
@@ -14,16 +16,19 @@ try {
     .usage('[options] <url1 ...> <url2 ...>')
     .arguments('[module]', 'prints module version from the node_modules')
     .option('-d, --debug', 'see full error messages, mostly for debugging')
-    .option('--disable-colors', 'minimize color and styling usage in output')
+    .option('--disable-colors', 'minimize color and styling usage in output');
 
   program.parse(process.argv);
 
-  if (program.args.length < 2) {
+  if (program.args.length < 1) {
     throw new Error('There should be two urls as an arguments')
   }
 
-  const [firstUrl, secondUrl] = program.args;
-  console.log('Urls to compare:', JSON.stringify({firstUrl, secondUrl}, null, 2));
+  const [firstUrl] = program.args;
+
+  traceWithScreenshots(firstUrl);
+
+  videoWithWebRTC(firstUrl);
 
 } catch (error) {
   handleError(error, program.debug);
