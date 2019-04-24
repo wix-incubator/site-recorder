a846e63f5515a1e478e81ee664fe509d0f24954c
Vitalii Sashyn <vitaliis@wix.com>
2019-04-24 18:23:45 +0300

    Add test to check that GIF is generated

---
+++ src/test/index.spec.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const { spawn } = require('child_process');
+
+const FILE_GIF = './mygif.gif';
+
+jest.setTimeout(20000000)
+
+test('Check that the new gif file is created', () => {
+  fs.unlinkSync(FILE_GIF) // before test remove gif if it's exists
+
+  const child = spawn('node', ['./src/cli.js', 'http://ronnyr34.wixsite.com/mysite-1', '--generate-gif']);
+
+  return new Promise((resolve, reject) => {
+     // debug
+    child.stdout.pipe(process.stdout);
+
+    child.on('exit', function (code, signal) {
+      console.log('child process exited with ' +
+                  `code ${code} and signal ${signal}`);
+        fs.access(FILE_GIF, fs.constants.F_OK, (err) => {
+          expect(err).toBe(null);
+          resolve();
+        });
+    });
+    child.on('error', function (code, signal) {
+      console.error('child process exited with ' +
+                  `code ${code} and signal ${signal}`);
+      reject();
+    });
+  })
+})
